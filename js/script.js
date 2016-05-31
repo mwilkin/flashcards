05db6bd9157b84a8975a2df3584f8236c8f00362
Cory Olson and Matt Wilkin <matt@>
2016-05-31 14:36:38 -0700

    added JS functionality.

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(function(){
+    $("p").hide();
+    $("#cardJSLink").click(function(){
+      $("#cardJSContent").toggle();
+    })
+
+    $("#cardOpLink").click(function(){
+      $("#cardOpContent").toggle();
+    })
+
+    $("#cardVarLink").click(function(){
+      $("#cardVarContent").toggle();
+    })
+
+    $("#cardconvLink").click(function(){
+      $("#cardconvContent").toggle();
+    })
+
+    $("#cardFuncLink").click(function(){
+      $("#cardFuncContent").toggle();
+    })
+
+    $("#cardMethLink").click(function(){
+      $("#cardMethContent").toggle();
+    })
+
+    $("#cardArgLink").click(function(){
+      $("#cardArgContent").toggle();
+    })
+
+    $("#cardParaLink").click(function(){
+      $("#cardParaContent").toggle();
+    })
+    $("#cardRetLink").click(function(){
+      $("#cardRetContent").toggle();
+    })
+
+    $("#cardChainLink").click(function(){
+      $("#cardChainContent").toggle();
+    })
+
+    $("#cardStrLink").click(function(){
+      $("#cardStrContent").toggle();
+    })
+
+    $("#cardBooLink").click(function(){
+      $("#cardBooContent").toggle();
+    })
+
+    $("#cardUndefLink").click(function(){
+      $("#cardUndefContent").toggle();
+    })
+
+    $("#cardNanLink").click(function(){
+      $("#cardNanContent").toggle();
+    })
+
+    $("#cardEscLink").click(function(){
+      $("#cardEscContent").toggle();
+    })
+
+    $("#cardAlertLink").click(function(){
+      $("#cardAlertContent").toggle();
+    })
+
+    $("#cardCommLink").click(function(){
+      $("#cardCommContent").toggle();
+    })
+
+    $("#cardjQuerLink").click(function(){
+      $("#cardjQuerContent").toggle();
+    })
+
+    $("#cardAttribLink").click(function(){
+      $("#cardAttribContent").toggle();
+    })
+})
